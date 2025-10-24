6cae84b616918f1431d7acc0b20f38c82bb60596
AndriudDev <andriudev22@gmail.com>
2025-10-24 19:43:21 -0300

    1

--- js/script.js
+++ js/script.js
@@ -1,38 +1,6 @@
-// Detecta scroll para animaciones fade-in
-/* const sections = document.querySelectorAll('section');
 
-function checkVisibility() {
-  const triggerBottom = window.innerHeight * 0.85;
 
-  sections.forEach(section => {
-    const top = section.getBoundingClientRect().top; // posición superior del elemento
-    if (top < triggerBottom) {
-      section.classList.add('visible');
-
-      // Animar títulos letra por letra
-      const titles = section.querySelectorAll('h2, h1'); // Selecciona h2, h1 y titulo
-      titles.forEach(title => { // Animar cada título
-        if (!title.classList.contains('title-anim')) { // Evita reanimar
-          title.classList.add('title-anim');
-          const letters = title.textContent.split('');
-          title.textContent = '';
-          letters.forEach((letter, i) => { //
-            const span = document.createElement('span');
-            span.textContent = letter;
-            span.style.animationDelay = `${i * 0.05}s`; // más rápido y más sutil
-            title.appendChild(span);
-          });
-        }
-      });
-    }
-  });
-} 
-
-window.addEventListener('scroll', checkVisibility);
-window.addEventListener('load', checkVisibility);
-*/
-
-// Animación de revelado con retraso escalonado
+//! Animación de revelado con retraso escalonado
 function revealOnScroll() {
   const reveals = document.querySelectorAll('.reveal');
   const windowHeight = window.innerHeight;
